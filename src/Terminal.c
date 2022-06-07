437d52c5aa69d17a40d0e71ccc7919949cc16b3b
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-06-07 16:53:09 +0200

    Added Stream versions of standard io

--- src/Terminal.c
+++ src/Terminal.c
@@ -1,1 +1,40 @@
 #include "Terminal.h"
+
+#include "Stream.h"
+
+// FILE* stream functions defined in Stream.c
+size_t _stFRead(void* stream, char* buffer, size_t length);
+size_t _stFWrite(void* stream, const char* data, size_t length);
+size_t _stFSeek(void* stream, long offset, int pos);
+
+Stream _termCreate(FILE* str, StreamFlags flags);
+int _nothing(void* p) { return 0; }
+
+Stream _termIn()
+{
+    static Stream s = { .flags = stNONE };
+    if (s.flags == stNONE)
+        s = _termCreate(stdin, stREAD);
+    return s;
+}
+
+Stream _termOut()
+{
+    static Stream s = { .flags = stNONE };
+    if (s.flags == stNONE)
+        s = _termCreate(stdout, stWRITE);
+    return s;
+}
+
+Stream _termErr()
+{
+    static Stream s = { .flags = stNONE };
+    if (s.flags == stNONE)
+        s = _termCreate(stderr, stWRITE);
+    return s;
+}
+
+Stream _termCreate(FILE* str, StreamFlags flags)
+{
+    return stCreate(flags, str, _stFWrite, _stFRead, _stFSeek, _nothing);
+}
--- src/Terminal.h
+++ src/Terminal.h
@@ -1,6 +1,12 @@
 #ifndef TERMINAL_INCLUDED
 #define TERMINAL_INCLUDED
 
+#include "Stream.h"
+
+#define term_in _termIn()
+#define term_out _termOut()
+#define term_err _termErr()
+
 #define term_COLRESET "\x1b[0m"
 
 #define term_BBLACK "\x1b[90m"
@@ -11,5 +17,9 @@
 #define term_BMAGENTA "\x1b[95m"
 #define term_BCYAN "\x1b[96m"
 #define term_BWHITE "\x1b[97m"
+
+Stream _termIn();
+Stream _termOut();
+Stream _termErr();
 
 #endif // TERMINAL_INCLUDED
